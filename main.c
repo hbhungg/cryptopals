dab48b83e73a535285c4bd27d09d5da9acd0640b
hung <bahung1tt@gmail.com>
2024-01-05 13:22:31 +0700

    fixup

--- main.c
+++ main.c
@@ -13,32 +13,36 @@ char base64_char(int val) {
   return '?';
 }
 
-char input[] = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-char result[] = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
 
 void hex_to_base64(char* base64, char *hex) {
-  for (int i=0;i<strlen(input);i+=3) {
-    // input[i:i+3]
+  for (int i=0;i<strlen(hex);i+=3) {
+    // hex[i:i+3]
     char slice[4];
-    strncpy(slice, input+i, 3);
-    slice[3] = '\0';
+    strncpy(slice, hex+i, 3);
+    slice[3] = '\0'; // NULL terminate the slice, else strtol will go crazy
 
+    // String to hex
     int buffer = strtol(slice, NULL, 16);
 
-    int b1 = buffer >> 6;
-    int b2 = buffer & (1 << 6)-1;
-    char c1 = base64_char(b1);
-    char c2 = base64_char(b2);
-    base64[(i/3)*2] = c1;
-    base64[(i/3)*2+1] = c2;
+    // Buffer is 12 bit, base64 is 6 bit each
+    // So we split the buffer, decode them to char, and write to retval buffer
+    base64[(i/3)*2] = base64_char(buffer >> 6);
+    base64[(i/3)*2+1] = base64_char(buffer & (1 << 6) - 1);
   }
 }
 
+
 int main() {
+  char input[] = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+  char result[] = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+
   printf("%s\n", input);
+
   char *retval = (char*)malloc(64);
   hex_to_base64(retval, input);
+
   printf("%s\n", retval);
   printf("%s\n", result);
+
   free(retval);
 }
